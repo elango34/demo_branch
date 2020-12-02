e092f3deb4d8c7c95619382d6583c4814c9b9c37
Elango R <elangoravi7@gmail.com>
2020-12-02 21:11:21 +0530

    added new line

--- demo.java
+++ demo.java
@@ -3,5 +3,6 @@ public static void main(String[] args) {
         System.out.println("this is a demo file ...");
         System.out.println("added through feature branch...");
         System.out.println("added through feature branch 2...");
+        System.out.println("added new line in master intermediatry after creats branch");
         }
         }
